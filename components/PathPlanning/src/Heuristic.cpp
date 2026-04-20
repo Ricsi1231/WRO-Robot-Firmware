251750d942dd74adf17f30984eee56ee8123cf90
Ricsi1231 <richardnagy551@gmail.com>
2026-04-20 18:05:37 +0200

    feat(PathPlanning): add A* based Path Planning algorithim

---
+++ components/PathPlanning/src/Heuristic.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file Heuristic.cpp
+ * @brief Implementation of heuristic distance functions.
+ */
+
+#include "Heuristic.hpp"
+
+#include <algorithm>
+#include <cmath>
+
+namespace WroRobotSoftware {
+namespace PathPlanning {
+
+uint32_t Heuristic::manhattan(GridPosition source, GridPosition target) {
+  int32_t dx = std::abs(source.x - target.x);
+  int32_t dy = std::abs(source.y - target.y);
+  return static_cast<uint32_t>(dx + dy);
+}
+
+uint32_t Heuristic::euclidean(GridPosition source, GridPosition target) {
+  int64_t dx = std::abs(source.x - target.x);
+  int64_t dy = std::abs(source.y - target.y);
+  return static_cast<uint32_t>(std::sqrt(static_cast<double>(dx * dx + dy * dy)) + 0.5);
+}
+
+uint32_t Heuristic::octagonal(GridPosition source, GridPosition target) {
+  int32_t dx = std::abs(source.x - target.x);
+  int32_t dy = std::abs(source.y - target.y);
+  return static_cast<uint32_t>((dx + dy) - std::min(dx, dy));
+}
+
+}  // namespace PathPlanning
+}  // namespace WroRobotSoftware
